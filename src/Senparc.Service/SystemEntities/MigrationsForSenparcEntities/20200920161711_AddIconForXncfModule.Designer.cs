74cbbf5637a2c140cebfcf1aabafa7644a09ac4d
JeffreySu <www.jeffrey.su@gmail.com>
2020-09-21 00:26:01 +0800

    完成数据库生成重构（本次更新需要使用 update-database 手动同步数据库）

--- src/Senparc.Service/SystemEntities/MigrationsForSenparcEntities/20200920161711_AddIconForXncfModule.Designer.cs
+++ src/Senparc.Service/SystemEntities/MigrationsForSenparcEntities/20200920161711_AddIconForXncfModule.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Senparc.Core.Models;
 
-namespace Senparc.Web.Migrations
+namespace Senparc.Service.SystemEntities.MigrationsForSenparcEntities
 {
     [DbContext(typeof(SenparcEntities))]
-    partial class SenparcEntitiesModelSnapshot : ModelSnapshot
+    [Migration("20200920161711_AddIconForXncfModule")]
+    partial class AddIconForXncfModule
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
