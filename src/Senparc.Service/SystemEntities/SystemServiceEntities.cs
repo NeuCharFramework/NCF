4697ae256af4bc4d40cebe60b0c4ce829e5b16cd
JeffreySu <www.jeffrey.su@gmail.com>
2020-10-10 22:56:58 +0800

    升级重构后的多数据库系统

--- src/Senparc.Service/SystemEntities/SystemServiceEntities.cs
+++ src/Senparc.Service/SystemEntities/SystemServiceEntities.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using Senparc.Core.Models;
+using Senparc.Ncf.Database;
+using Senparc.Ncf.Database.MultipleMigrationDbContext;
 using Senparc.Ncf.XncfBase;
 using Senparc.Ncf.XncfBase.Database;
 using System;
@@ -12,9 +14,9 @@ namespace Senparc.Service
     /// 当前 Entities 只为对接 Service 的 Register 而存在（必须继承自 XncfDatabaseDbContext），没有特别的意义。
     /// TODO：让 SenparcEntities 继承 XncfDatabaseDbContext，并提供 Senparc.Core 的 Register。
     /// </summary>
+    [MultipleMigrationDbContext(MultipleDatabaseType.SQLite, typeof(Senparc.Service.Register))]
     public class SystemServiceEntities : XncfDatabaseDbContext
     {
-        public override IXncfDatabase XncfDatabaseRegister => new Register();
         public SystemServiceEntities(DbContextOptions dbContextOptions) : base(dbContextOptions)
         {
         }
