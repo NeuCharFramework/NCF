32b707f388c3b89931b10074c1ac940a7aec277e
淳冉 <455274752@qq.com>
2020-11-21 23:02:12 +0800

    订正

--- src/Senparc.Areas.Admin/wwwroot/js/Admin/Pages/XncfModule/index.js
+++ src/Senparc.Areas.Admin/wwwroot/js/Admin/Pages/XncfModule/index.js
@@ -21,7 +21,7 @@
     watch: {
         'isExtend': {
             handler: function (val, oldVal) {
-                this.handlerText = val ? '开启【扩展模块】管理模式' : '切换至发布状态，隐藏【扩展莫管】管理单元';
+                this.handlerText = val ? '开启【扩展模块】管理模式' : '切换至发布状态，隐藏【扩展模块】管理单元';
                 this.handlerTips = val ? '打开【扩展模块】管理功能后，所有扩展模块将显示在【扩展模块】二级目录中。确定要打开吗？' : '隐藏【扩展模块】管理功能后，所有扩展模块将并列显示在一级目录中。如需重新打开，请直接浏览器内访问此页面【/Admin/XncfModule】。确定要隐藏吗？';
             },
             immediate: true
