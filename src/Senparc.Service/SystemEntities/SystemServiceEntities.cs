d47b98088df140a8ca43976c67945cb81fe2739a
JeffreySu <www.jeffrey.su@gmail.com>
2021-02-10 00:38:58 +0800

    升级基础版本，把 SQLite 放到独立的 SystemServiceEntities 中

--- src/Senparc.Service/SystemEntities/SystemServiceEntities.cs
+++ src/Senparc.Service/SystemEntities/SystemServiceEntities.cs
@@ -14,7 +14,6 @@ namespace Senparc.Service
     /// <summary>
     /// 当前 Entities 只为帮助 SenparcEntities 生成 Migration 信息而存在，没有特别的操作意义。
     /// </summary>
-    [MultipleMigrationDbContext(MultipleDatabaseType.SQLite, typeof(Senparc.Service.Register))]
     public class SystemServiceEntities : SenparcEntities
     {
         public SystemServiceEntities(DbContextOptions/*<SystemServiceEntities>*/ dbContextOptions) : base(dbContextOptions)
