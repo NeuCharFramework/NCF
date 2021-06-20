63f09c7a127db3ca3e9f36bf36139e79f7ef7082
JeffreySu <www.jeffrey.su@gmail.com>
2021-06-20 23:41:20 +0800

    重构并更新WebApi

--- src/Senparc.Web/Startup.cs
+++ src/Senparc.Web/Startup.cs
@@ -6,6 +6,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Options;
+using Microsoft.OpenApi.Models;
 using Senparc.CO2NET;
 using Senparc.Ncf.Core.Models;
 using Senparc.Ncf.Core.MultiTenant;
@@ -14,7 +15,14 @@ using Senparc.Ncf.Database;
 //using Senparc.Ncf.Database.Sqlite;//根据需要添加或删除，使用需要引用 Senparc.Ncf.Database.Sqlite
 using Senparc.Ncf.Database.SqlServer;//根据需要添加或删除，使用需要引用 Senparc.Ncf.Database.SqlServer
 using Senparc.Ncf.Service.MultiTenant;
+using Senparc.NeuChar;
 using Senparc.Web.Hubs;
+using Senparc.Xncf.WeixinManager.Services;
+using Swashbuckle.AspNetCore.Annotations;
+using Swashbuckle.AspNetCore.SwaggerGen;
+using System;
+using System.Linq;
+using System.Reflection;
 
 namespace Senparc.Web
 {
@@ -65,6 +73,7 @@ namespace Senparc.Web
 
             #endregion
 
+
             app.UseHttpsRedirection();
             app.UseStaticFiles();
 
@@ -75,6 +84,7 @@ namespace Senparc.Web
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapRazorPages();
+                endpoints.MapControllers();
             });
 
             //Use NCF（必须）
