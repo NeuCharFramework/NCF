762db97c809d6f7869ed7d1c2eb071af19b54cf2
JeffreySu <www.jeffrey.su@gmail.com>
2021-12-16 15:50:43 +0800

    modify

--- src/back-end/Senparc.Web/Register.cs
+++ src/back-end/Senparc.Web/Register.cs
@@ -23,7 +23,7 @@ namespace Senparc.Web
     /// </summary>
     public static class Register
     {
-        public static void AddNcfServices(this WebApplicationBuilder builder, IConfiguration configuration, IWebHostEnvironment env)
+        public static void AddNcf(this WebApplicationBuilder builder, IConfiguration configuration, IWebHostEnvironment env)
         {
             //如果运行在IIS中，需要添加IIS配置
             //https://docs.microsoft.com/zh-cn/aspnet/core/host-and-deploy/iis/index?view=aspnetcore-2.1&tabs=aspnetcore2x#supported-operating-systems
