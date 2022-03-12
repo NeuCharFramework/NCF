60130104e49b2e2aa5e099af80c40eea876bcd1c
JeffreySu <www.jeffrey.su@gmail.com>
2022-03-12 22:51:17 +0800

    客户端测试（有多余文件，不需要提交）

---
+++ src/back-end/Senparc.Xncf.Accounts/wwwroot/vue_module/admin/public/accounts/d.common.about.js
@@ -0,0 +1,163 @@
+((typeof self !== 'undefined' ? self : this)["webpackJsonpd"] = (typeof self !== 'undefined' ? self : this)["webpackJsonpd"] || []).push([[0],{
+
+/***/ "bb51":
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+
+// CONCATENATED MODULE: ./node_modules/cache-loader/dist/cjs.js?{"cacheDirectory":"node_modules/.cache/vue-loader","cacheIdentifier":"44ae0be2-vue-loader-template"}!./node_modules/vue-loader/lib/loaders/templateLoader.js??vue-loader-options!./node_modules/cache-loader/dist/cjs.js??ref--0-0!./node_modules/vue-loader/lib??vue-loader-options!./src/views/Home.vue?vue&type=template&id=5c2bca8e&
+var render = function () {var _vm=this;var _h=_vm.$createElement;var _c=_vm._self._c||_h;return _c('div',{staticClass:"app-container home"},[_c('h1',[_vm._v(_vm._s(_vm.title)+"1")]),_c('HelloWorld',{attrs:{"msg":"我是远程加载的!"}})],1)}
+var staticRenderFns = []
+
+
+// CONCATENATED MODULE: ./src/views/Home.vue?vue&type=template&id=5c2bca8e&
+
+// CONCATENATED MODULE: ./node_modules/cache-loader/dist/cjs.js?{"cacheDirectory":"node_modules/.cache/vue-loader","cacheIdentifier":"44ae0be2-vue-loader-template"}!./node_modules/vue-loader/lib/loaders/templateLoader.js??vue-loader-options!./node_modules/cache-loader/dist/cjs.js??ref--0-0!./node_modules/vue-loader/lib??vue-loader-options!./src/components/HelloWorld.vue?vue&type=template&id=34fefa9f&
+var HelloWorldvue_type_template_id_34fefa9f_render = function () {var _vm=this;var _h=_vm.$createElement;var _c=_vm._self._c||_h;return _c('div',{staticClass:"hello"},[_c('h1',[_vm._v(_vm._s(_vm.msg))]),_c('img',{staticStyle:{"width":"100px","height":"100px"},attrs:{"src":__webpack_require__("cf05"),"height":"200","width":"200"}})])}
+var HelloWorldvue_type_template_id_34fefa9f_staticRenderFns = []
+
+
+// CONCATENATED MODULE: ./src/components/HelloWorld.vue?vue&type=template&id=34fefa9f&
+
+// CONCATENATED MODULE: ./node_modules/cache-loader/dist/cjs.js??ref--12-0!./node_modules/thread-loader/dist/cjs.js!./node_modules/babel-loader/lib!./node_modules/cache-loader/dist/cjs.js??ref--0-0!./node_modules/vue-loader/lib??vue-loader-options!./src/components/HelloWorld.vue?vue&type=script&lang=js&
+//
+//
+//
+//
+//
+//
+//
+/* harmony default export */ var HelloWorldvue_type_script_lang_js_ = ({
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  }
+});
+// CONCATENATED MODULE: ./src/components/HelloWorld.vue?vue&type=script&lang=js&
+ /* harmony default export */ var components_HelloWorldvue_type_script_lang_js_ = (HelloWorldvue_type_script_lang_js_); 
+// EXTERNAL MODULE: ./node_modules/vue-loader/lib/runtime/componentNormalizer.js
+var componentNormalizer = __webpack_require__("2877");
+
+// CONCATENATED MODULE: ./src/components/HelloWorld.vue
+
+
+
+
+
+/* normalize component */
+
+var component = Object(componentNormalizer["a" /* default */])(
+  components_HelloWorldvue_type_script_lang_js_,
+  HelloWorldvue_type_template_id_34fefa9f_render,
+  HelloWorldvue_type_template_id_34fefa9f_staticRenderFns,
+  false,
+  null,
+  null,
+  null
+  
+)
+
+/* harmony default export */ var HelloWorld = (component.exports);
+// CONCATENATED MODULE: ./node_modules/cache-loader/dist/cjs.js??ref--12-0!./node_modules/thread-loader/dist/cjs.js!./node_modules/babel-loader/lib!./node_modules/cache-loader/dist/cjs.js??ref--0-0!./node_modules/vue-loader/lib??vue-loader-options!./src/views/Home.vue?vue&type=script&lang=js&
+//
+//
+//
+//
+//
+//
+//
+//
+//
+// @ is an alias to /src
+
+/* harmony default export */ var Homevue_type_script_lang_js_ = ({
+  name: 'home',
+  components: {
+    HelloWorld: HelloWorld
+  },
+  data: function data() {
+    return {
+      title: "vue-module"
+    };
+  }
+});
+// CONCATENATED MODULE: ./src/views/Home.vue?vue&type=script&lang=js&
+ /* harmony default export */ var views_Homevue_type_script_lang_js_ = (Homevue_type_script_lang_js_); 
+// CONCATENATED MODULE: ./src/views/Home.vue
+
+
+
+
+
+/* normalize component */
+
+var Home_component = Object(componentNormalizer["a" /* default */])(
+  views_Homevue_type_script_lang_js_,
+  render,
+  staticRenderFns,
+  false,
+  null,
+  null,
+  null
+  
+)
+
+/* harmony default export */ var Home = __webpack_exports__["default"] = (Home_component.exports);
+
+/***/ }),
+
+/***/ "f820":
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+
+// CONCATENATED MODULE: ./node_modules/cache-loader/dist/cjs.js?{"cacheDirectory":"node_modules/.cache/vue-loader","cacheIdentifier":"44ae0be2-vue-loader-template"}!./node_modules/vue-loader/lib/loaders/templateLoader.js??vue-loader-options!./node_modules/cache-loader/dist/cjs.js??ref--0-0!./node_modules/vue-loader/lib??vue-loader-options!./src/views/About.vue?vue&type=template&id=14a3c4fe&
+var render = function () {var _vm=this;var _h=_vm.$createElement;var _c=_vm._self._c||_h;return _vm._m(0)}
+var staticRenderFns = [function () {var _vm=this;var _h=_vm.$createElement;var _c=_vm._self._c||_h;return _c('div',{staticClass:"about"},[_c('h1',[_vm._v("我是about页面！")])])}]
+
+
+// CONCATENATED MODULE: ./src/views/About.vue?vue&type=template&id=14a3c4fe&
+
+// CONCATENATED MODULE: ./node_modules/cache-loader/dist/cjs.js??ref--12-0!./node_modules/thread-loader/dist/cjs.js!./node_modules/babel-loader/lib!./node_modules/cache-loader/dist/cjs.js??ref--0-0!./node_modules/vue-loader/lib??vue-loader-options!./src/views/About.vue?vue&type=script&lang=js&
+//
+//
+//
+//
+//
+//
+/* harmony default export */ var Aboutvue_type_script_lang_js_ = ({
+  name: 'about',
+  mounted: function mounted() {}
+});
+// CONCATENATED MODULE: ./src/views/About.vue?vue&type=script&lang=js&
+ /* harmony default export */ var views_Aboutvue_type_script_lang_js_ = (Aboutvue_type_script_lang_js_); 
+// EXTERNAL MODULE: ./node_modules/vue-loader/lib/runtime/componentNormalizer.js
+var componentNormalizer = __webpack_require__("2877");
+
+// CONCATENATED MODULE: ./src/views/About.vue
+
+
+
+
+
+/* normalize component */
+
+var component = Object(componentNormalizer["a" /* default */])(
+  views_Aboutvue_type_script_lang_js_,
+  render,
+  staticRenderFns,
+  false,
+  null,
+  null,
+  null
+  
+)
+
+/* harmony default export */ var About = __webpack_exports__["default"] = (component.exports);
+
+/***/ })
+
+}]);
+//# sourceMappingURL=d.common.about.js.map
