af2eeb5676e3ba1e483cd49bc3e1f4b3fa68ec74
JeffreySu <www.jeffrey.su@gmail.com>
2022-08-19 01:08:21 +0800

    add AdminSenparcEntities_Oracle

---
+++ src/back-end/Senparc.Xncf.Accounts/Domain/Models/MultipleDatabase/AccountSenparcEntities_Oracle.cs
@@ -0,0 +1,40 @@
+﻿
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Senparc.Ncf.Database;
+using Senparc.Xncf.Accounts.Domain.Models;
+using Senparc.Ncf.XncfBase.Database;
+using System;
+using System.IO;
+using Senparc.Ncf.Core.Models;
+
+namespace Senparc.Xncf.Accounts.Models
+{
+    [MultipleMigrationDbContext(MultipleDatabaseType.Oracle, typeof(Register))]
+    public class AccountSenparcEntities_Oracle : AccountSenparcEntities
+    {
+        public AccountSenparcEntities_Oracle(DbContextOptions<AccountSenparcEntities_Oracle> dbContextOptions) : base(dbContextOptions)
+        {
+        }
+    }
+    
+
+    /// <summary>
+    /// 设计时 DbContext 创建（仅在开发时创建 Code-First 的数据库 Migration 使用，在生产环境不会执行）
+    /// <para>1、切换至 Debug 模式</para>
+    /// <para>2、运行：PM> add-migration [更新名称] -c AccountSenparcEntities_Oracle -o Domain/Migrations/Migrations.Oracle </para>
+    /// </summary>
+    public class SenparcDbContextFactory_Oracle : SenparcDesignTimeDbContextFactoryBase<AccountSenparcEntities_Oracle, Register>
+    {
+        protected override Action<IServiceCollection> ServicesAction => services =>
+        {
+            //指定其他数据库
+            services.AddDatabase("Senparc.Ncf.Database.Oracle", "Senparc.Ncf.Database.Oracle", "OracleDatabaseConfiguration");
+        };
+
+        public SenparcDbContextFactory_Oracle() : base(SenparcDbContextFactoryConfig.RootDictionaryPath)
+        {
+
+        }
+    }
+}
