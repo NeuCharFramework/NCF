3a885d4521c752906217f371aac900d2100a8d8a
JeffreySu <www.jeffrey.su@gmail.com>
2022-08-28 18:57:19 +0800

    添加单元测试演示

--- src/back-end/Senparc.Xncf.WeixinManagerBase.Tests/TestBase.cs
+++ src/back-end/Senparc.Xncf.WeixinManagerBase.Tests/TestBase.cs
@@ -1,15 +1,18 @@
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Moq;
 using Senparc.CO2NET;
 using Senparc.CO2NET.RegisterServices;
+using Senparc.Ncf.XncfBase;
+using System.Text;
 
 namespace Senparc.Xncf.WeixinManagerBase.Tests
 {
     [TestClass]
     public class TestBase
     {
-        public IServiceCollection ServiceCollection { get; }
+        public static IServiceCollection ServiceCollection { get; private set; }
         public static IConfiguration Configuration { get; set; }
 
         public static IHostEnvironment Env { get; set; }
@@ -17,9 +20,60 @@ namespace Senparc.Xncf.WeixinManagerBase.Tests
         protected static IRegisterService registerService;
         protected static SenparcSetting _senparcSetting;
 
+
         public TestBase()
         {
+            Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
 
+            RegisterServiceCollection();
+
+
+            RegisterServiceStart();
+
+
+        }
+
+        /// <summary>
+        /// ×¢²á IServiceCollection ºÍ MemoryCache
+        /// </summary>
+        public static void RegisterServiceCollection()
+        {
+            ServiceCollection = new ServiceCollection();
+            var configBuilder = new ConfigurationBuilder();
+            configBuilder.AddJsonFile("appsettings.json", false, false);
+            var config = configBuilder.Build();
+            Configuration = config;
+
+            _senparcSetting = new SenparcSetting() { IsDebug = true };
+            config.GetSection("SenparcSetting").Bind(_senparcSetting);
+
+            ServiceCollection.AddSenparcGlobalServices(config);
+            ServiceCollection.AddMemoryCache();//Ê¹ÓÃÄÚ´æ»º´æ
+        }
+
+        /// <summary>
+        /// ×¢²á RegisterService.Start()
+        /// </summary>
+        public static void RegisterServiceStart(bool autoScanExtensionCacheStrategies = false)
+        {
+            //×¢²á
+            var mockEnv = new Mock<IHostEnvironment/*IHostingEnvironment*/>();
+            mockEnv.Setup(z => z.ContentRootPath).Returns(() => UnitTestHelper.RootPath);
+
+            Env = mockEnv.Object;
+
+            registerService = Senparc.CO2NET.AspNet.RegisterServices.RegisterService.Start(Env, _senparcSetting)
+                .UseSenparcGlobal(autoScanExtensionCacheStrategies);
+
+            StartNcfEngine();
+
+        }
+
+        private static void StartNcfEngine()
+        {
+            var result = ServiceCollection.StartEngine(TestBase.Configuration, TestBase.Env);
+            Console.WriteLine(result);
+            //Assert.IsTrue(Senparc.Ncf.XncfBase.XncfRegisterManager.RegisterList.Count > 0);
         }
     }
 }
---
+++ src/back-end/Senparc.Xncf.WeixinManagerBase.Tests/UserTests.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Senparc.NeuChar.App.AppStore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Senparc.Xncf.WeixinManagerBase.Tests
+{
+    [TestClass]
+    public class UserTests : TestBase
+    {
+        //init
+
+        //private Viewer GenerateNewViewer(string userName = null, string password = null)
+        //{
+        //    userName ??= new Random().Next(1000).ToString();
+        //    password ??= (string)null;
+
+        //    var viewer = new Viewer(userName, password);
+        //    return viewer;
+        //}
+
+        //[TestMethod]
+        //public void UserCreateTest()
+        //{
+        //    var userName = new Random().Next(1000).ToString();
+        //    var password = (string)null;
+
+        //    var viewer = GenerateNewViewer(userName, password);
+
+        //    Assert.AreEqual(userName, viewer.UserName);
+        //    Assert.AreEqual(password, viewer.Password);
+
+        //    //Assert.AreEqual(false, viewer.GetType().GetProperty(nameof(viewer.UserName)));
+
+        //    //int result = viewer.UpdatePassword(password);
+        //}
+
+        //[TestMethod]
+        //public void ChangePassword()
+        //{
+        //    var viewer = GenerateNewViewer();
+
+        //    var newPassword = Guid.NewGuid().ToString("n");
+        //    Operation result = viewer.UpdatePassword(newPassword);
+
+        //    Assert.AreEqual(Operation.Success, result);
+        //    Assert.AreEqual(newPassword, viewer.Password);
+        //}
+    }
+}
