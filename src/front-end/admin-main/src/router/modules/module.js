cbbfb8eb152ec016adecc6026f8d69b7a516d487
lightShao <1298836090@qq.com>
2022-10-26 15:53:23 +0800

    前端修改

--- src/front-end/admin-main/src/router/modules/module.js
+++ src/front-end/admin-main/src/router/modules/module.js
@@ -1,36 +1,24 @@
-// /** When your routing table is too long, you can split it into small modules**/
-// import Layout from '@/layout'
+/** When your routing table is too long, you can split it into small modules**/
+import Layout from '@/layout'
 
-// const moduleRouter = {
-//   path: '/XncfModule',
-//   component: Layout,
-//   redirect: '/Admin/XncfModule/Index',
-//   // 这里的name需要和module模块中module.js的addRoute的name相同
-//   name: 'XncfModule',
-//   meta: {
-//     title: '扩展模块',
-//     icon: 'el-icon-cpu'
-//   },
-//   children: [
-//     {
-//       path: '/Admin/XncfModule/Index',
-//       component: () => import('@/views/module/index'),
-//       name: 'XncfModuleIndex',
-//       meta: { title: '模块管理'}
-//     },
-//     {
-//       path: '/Admin/XncfModule/Start',
-//       component: () => import('@/views/module/start'),
-//       name: 'XncfModuleStart',
-//       meta: { title: '执行'}
-//     },
-//     // {
-//     //   path: '/Admin/XncfModule/Start',
-//     //   component: () => import('@/views/module/start'),
-//     //   name: 'XncfModuleStart',
-//     //   meta: { title: '执行'}
-//     // }
-//   ]
-// }
+const moduleRouter = {
+  path: '/Module',
+  component: Layout,
+  redirect: '/Module/home',
+  // 这里的name需要和module模块中module.js的addRoute的name相同
+  name: 'Module',
+  meta: {
+    title: '拓展模块',
+    icon: 'el-icon-cpu'
+  },
+  children: [
+    {
+      path: '/Module/home',
+      component: () => import('@/views/module/home'),
+      name: 'ModuleHome',
+      meta: { title: '拓展模块'}
+    }
+  ]
+}
 
-// export default moduleRouter
+export default moduleRouter
--- src/front-end/admin-main/src/views/Admin/XncfModule/Index.vue
+++ src/front-end/admin-main/src/views/Admin/XncfModule/Index.vue
@@ -1,11 +1,7 @@
 <template>
   <div class="moduleIndex">
     <div class="filter-container" style="text-align: right">
-      <el-popconfirm
-        placement="top"
-        :title="handlerTips"
-        @confirm="handleSwitch"
-      >
+      <el-popconfirm placement="top" :title="handlerTips" @confirm="handleSwitch">
         <el-button size="mini" type="danger" slot="reference">{{
           handlerText
         }}</el-button>
@@ -13,10 +9,8 @@
     </div>
     <el-container v-if="newTableData.length > 0">
       <el-header class="module-header">
-        <span class="start-title"
-          >发现 {{ newTableData.length }} 个新模块！</span
-        ></el-header
-      >
+        <span class="start-title">发现 {{ newTableData.length }} 个新模块！</span>
+      </el-header>
       <el-main>
         <el-table :data="newTableData" style="width: 100%; margin-bottom: 20px">
           <el-table-column align="left" label="模块名称">
@@ -37,11 +31,7 @@
               {{ scope.row.version }}
             </template>
           </el-table-column>
-          <el-table-column
-            align="center"
-            label="唯一编码（全局唯一）"
-            width="350"
-          >
+          <el-table-column align="center" label="唯一编码（全局唯一）" width="350">
             <template slot-scope="scope">
               <i class="fa fa-shield"></i>
               {{ scope.row.uid }}
@@ -49,13 +39,7 @@
           </el-table-column>
           <el-table-column label="操作" width="120" align="center">
             <template slot-scope="scope">
-              <el-button
-                size="mini"
-                type="primary"
-                icon="el-icon-circle-plus"
-                @click="handleInstall(scope.$index, scope.row)"
-                >安装</el-button
-              >
+              <el-button size="mini" type="primary" icon="el-icon-circle-plus" @click="handleInstall(scope.$index, scope.row)">安装</el-button>
             </template>
           </el-table-column>
         </el-table>
@@ -63,22 +47,16 @@
     </el-container>
     <el-container v-if="oldTableData.length > 0">
       <el-header class="module-header">
-        <span class="start-title">已安装模块</span></el-header
-      >
+        <span class="start-title">已安装模块</span>
+      </el-header>
       <el-main>
-        <el-table
-          :data="oldTableData"
-          v-if="oldTableData.length > 0"
-          style="width: 100%; margin-bottom: 20px"
-        >
+        <el-table :data="oldTableData" v-if="oldTableData.length > 0" style="width: 100%; margin-bottom: 20px">
           <el-table-column align="left" label="菜单名称/版本号">
             <template slot-scope="scope">
               <div>
-                <i
-                  :class="
+                <i :class="
                     scope.row.icon ? scope.row.icon : 'fa fa-chain-broken'
-                  "
-                ></i>
+                  "></i>
                 {{ scope.row.menuName }}
               </div>
               <div>
@@ -87,11 +65,7 @@
               </div>
             </template>
           </el-table-column>
-          <el-table-column
-            align="center"
-            label="模块名称/唯一编码（全局唯一）"
-            width="350"
-          >
+          <el-table-column align="center" label="模块名称/唯一编码（全局唯一）" width="350">
             <template slot-scope="scope">
               <div>
                 <i class="fa fa-cubes"></i>
@@ -115,22 +89,8 @@
           </el-table-column>
           <el-table-column label="操作" width="250" align="center">
             <template slot-scope="scope">
-              <el-button
-                size="mini"
-                icon="el-icon-s-tools"
-                type="primary"
-                v-if="scope.row"
-                @click="handleHandle(scope.$index, scope.row)"
-                >操作</el-button
-              >
-              <el-button
-                size="mini"
-                v-if="scope.row.homeUrl"
-                icon="el-icon-s-home"
-                type="primary"
-                @click="handleIndex(scope.$index, scope.row)"
-                >主页</el-button
-              >
+              <el-button size="mini" icon="el-icon-s-tools" type="primary" v-if="scope.row" @click="handleHandle(scope.$index, scope.row)">操作</el-button>
+              <el-button size="mini" v-if="scope.row.homeUrl" icon="el-icon-s-home" type="primary" @click="handleIndex(scope.$index, scope.row)">主页</el-button>
               <span v-if="!scope.row"> 此模块已被删除~ </span>
             </template>
           </el-table-column>
@@ -141,290 +101,305 @@
 </template>
 
 <script>
-import { getModuleList, getUnModuleList,moduleInstallXncf } from "@/api/module";
+import { getModuleList, getUnModuleList, moduleInstallXncf } from '@/api/module'
+import { openHideManager, getSystemConfig } from '@/api/system'
 export default {
   data() {
     return {
       newTableData: [], // 新模块数据
       oldTableData: [], // 已安装模块
       isExtend: false, //是否切换状态
-      handlerText: "",
-      handlerTips: "",
-      newData: {},
-    };
+      handlerText: '',
+      handlerTips: '',
+      newData: {}
+    }
   },
   watch: {
     isExtend: {
       handler: function (val, oldVal) {
         this.handlerText = val
-          ? "开启【扩展模块】管理模式"
-          : "切换至发布状态，隐藏【扩展模块】管理单元";
+          ? '开启【扩展模块】管理模式'
+          : '切换至发布状态，隐藏【扩展模块】管理单元'
         this.handlerTips = val
-          ? "打开【扩展模块】管理功能后，所有扩展模块将显示在【扩展模块】二级目录中。确定要打开吗？"
-          : "隐藏【扩展模块】管理功能后，所有扩展模块将并列显示在一级目录中。如需重新打开，请直接浏览器内访问此页面【/Admin/XncfModule】。确定要隐藏吗？";
+          ? '打开【扩展模块】管理功能后，所有扩展模块将显示在【扩展模块】二级目录中。确定要打开吗？'
+          : '隐藏【扩展模块】管理功能后，所有扩展模块将并列显示在一级目录中。如需重新打开，请直接浏览器内访问此页面【/Admin/XncfModule】。确定要隐藏吗？'
       },
-      immediate: true,
-    },
+      immediate: true
+    }
   },
   filters: {
     oldDataState(value) {
       let obj = {
-        0: "关闭",
-        1: "开放",
-        2: "新增待审核",
-        3: "更新待审核",
-      };
-      return obj[value] || "";
-    },
+        0: '关闭',
+        1: '开放',
+        2: '新增待审核',
+        3: '更新待审核'
+      }
+      return obj[value] || ''
+    }
   },
   created: function () {
-    this.getList();
+    this.getList()
   },
   methods: {
     // 获取
     async getList() {
+      // 模块状态 是否切换状态
+
+      const extend = await getSystemConfig()
+      this.isExtend = extend.data[0].hideModuleManager
+      // console.log('isExtend',this.isExtend);
       // 已安装模块
-      const oldTableData = await getModuleList();
+      const oldTableData = await getModuleList()
       // console.log("oldTableData", oldTableData);
-      this.oldTableData = oldTableData.data || [];
-      // 是否切换状态
-      // this.isExtend = oldTableData.hideModuleManager
+      this.oldTableData = oldTableData.data || []
+
       // 未安装模块
-      const newTableData = await getUnModuleList();
+      const newTableData = await getUnModuleList()
       // console.log("newTableData", newTableData);
-      this.newTableData = newTableData.data || [];
+      this.newTableData = newTableData.data || []
     },
 
     // 切换状态
     async handleSwitch() {
-      console.log("dasdd");
-      await service.post("/Admin/XncfModule/Index?handler=HideManager");
-      this.isExtend = !this.isExtend;
-      window.location.href = "/Admin/Index";
+      // console.log('dasdd')
+      await openHideManager({ id: 0, hide: !this.isExtend })
+        .then((res) => {
+          // console.log('切换状态', res.data)
+          if(res){
+            this.isExtend = res.data.hideModuleManager
+            this.getList()
+          }
+        })
     },
 
     // 安装
     async handleInstall(index, row) {
-      await moduleInstallXncf(row.uid).then((res) => {
-          console.log("ok", res);
-          // 跳转到模块详情
-          this.$router.push("/Admin/XncfModule/Start/uid=" + row.uid);
-        }).catch(() => {
-          console.log("err");
-        });
+      await moduleInstallXncf(row.uid)
+        .then((res) => {
+          console.log('安装 ok', row.uid)
+          if (res.success) {
+            // 跳转到模块详情
+            this.$router.push('/Admin/XncfModule/Start/uid=' + row.uid)
+          }
+        })
+        .catch(() => {
+          this.$message.error('安装失败')
+        })
       // window.sessionStorage.setItem("setNavMenuActive", row.menuName);
       // 修改侧边栏
-      
     },
 
     // 操作
     handleHandle(index, row) {
-      this.$router.push("/Admin/XncfModule/Start/uid=" + row.uid);
+      this.$router.push('/Admin/XncfModule/Start/uid=' + row.uid)
       // window.location.href = "/Admin/XncfModule/Start/?uid=" + row.uid;
     },
+
     // 主页
     handleIndex(index, row) {
-      window.location.href = row.homeUrl;
-      // this.$router.push("/Admin/XncfModule/Start/uid=" + row.uid);
-    },
-  },
-};
+      console.log('主页', row)
+      window.location.href = row.homeUrl
+      // this.$router.push('/');
+    }
+  }
+}
 </script>
 
 <style lang="scss">
-.moduleIndex {
-  padding: 20px;
-}
+  .moduleIndex {
+    padding: 20px;
+  }
 
-.filter-container .float-right {
-  float: right;
-}
+  .filter-container .float-right {
+    float: right;
+  }
 
-.start-delete {
-  float: right;
-}
+  .start-delete {
+    float: right;
+  }
 
-.el-collapse-item__header {
-  background-color: #ebeef5;
-  padding-left: 15px;
-}
+  .el-collapse-item__header {
+    background-color: #ebeef5;
+    padding-left: 15px;
+  }
 
-.el-main .el-card {
-  margin: 10px 0;
-  max-width: 1000px;
-}
+  .el-main .el-card {
+    margin: 10px 0;
+    max-width: 1000px;
+  }
 
-.box-card .box-card-action {
-  cursor: pointer;
-  background-color: #ff6a00;
-  float: right;
-  position: relative;
-  white-space: nowrap;
-  line-height: 25px;
-  height: 25px;
-  color: #fff;
-  margin-left: 12.5px;
-  margin-right: 10px;
-  padding-left: 12.5px;
-  padding-right: 9.33333px;
-  width: 59px;
-  font-size: 14px;
-  text-align: right;
-}
+  .box-card .box-card-action {
+    cursor: pointer;
+    background-color: #ff6a00;
+    float: right;
+    position: relative;
+    white-space: nowrap;
+    line-height: 25px;
+    height: 25px;
+    color: #fff;
+    margin-left: 12.5px;
+    margin-right: 10px;
+    padding-left: 12.5px;
+    padding-right: 9.33333px;
+    width: 59px;
+    font-size: 14px;
+    text-align: right;
+  }
 
-.box-card .box-card-action2 {
-  cursor: pointer;
-  background-color: #909399;
-  float: right;
-  position: relative;
-  white-space: nowrap;
-  line-height: 25px;
-  height: 25px;
-  color: #fff;
-  margin-left: 12.5px;
-  margin-right: 10px;
-  padding-left: 12.5px;
-  padding-right: 9.33333px;
-  width: 59px;
-  font-size: 14px;
-  text-align: right;
-}
+  .box-card .box-card-action2 {
+    cursor: pointer;
+    background-color: #909399;
+    float: right;
+    position: relative;
+    white-space: nowrap;
+    line-height: 25px;
+    height: 25px;
+    color: #fff;
+    margin-left: 12.5px;
+    margin-right: 10px;
+    padding-left: 12.5px;
+    padding-right: 9.33333px;
+    width: 59px;
+    font-size: 14px;
+    text-align: right;
+  }
 
-.box-card .box-card-action::before {
-  content: "";
-  position: absolute;
-  left: 0;
-  width: 17.67767px;
-  background-color: #ff6a00;
-  height: 17.67767px;
-  transform: rotate(45deg);
-  transform-origin: top left;
-}
+  .box-card .box-card-action::before {
+    content: '';
+    position: absolute;
+    left: 0;
+    width: 17.67767px;
+    background-color: #ff6a00;
+    height: 17.67767px;
+    transform: rotate(45deg);
+    transform-origin: top left;
+  }
 
-.box-card .box-card-action2::before {
-  content: "";
-  position: absolute;
-  left: 0;
-  width: 17.67767px;
-  background-color: #909399;
-  height: 17.67767px;
-  transform: rotate(45deg);
-  transform-origin: top left;
-}
+  .box-card .box-card-action2::before {
+    content: '';
+    position: absolute;
+    left: 0;
+    width: 17.67767px;
+    background-color: #909399;
+    height: 17.67767px;
+    transform: rotate(45deg);
+    transform-origin: top left;
+  }
 
-.box-card .el-collapse-item__wrap {
-  background-color: #ebeef5;
-}
+  .box-card .el-collapse-item__wrap {
+    background-color: #ebeef5;
+  }
 
-.box-card .el-table th,
-.box-card .el-table tr {
-  background-color: #ebeef5;
-}
+  .box-card .el-table th,
+  .box-card .el-table tr {
+    background-color: #ebeef5;
+  }
 
-.box-card .no-content {
-  text-align: center;
-}
+  .box-card .no-content {
+    text-align: center;
+  }
 
-.start-green {
-  color: #67c23a;
-}
+  .start-green {
+    color: #67c23a;
+  }
 
-.start-gray {
-  color: #909399;
-}
+  .start-gray {
+    color: #909399;
+  }
 
-.start-warning {
-  color: #e6a23c;
-}
+  .start-warning {
+    color: #e6a23c;
+  }
 
-.start-danger {
-  color: #f56c6c;
-}
+  .start-danger {
+    color: #f56c6c;
+  }
 
-.start-des {
-  font-size: 13px;
-}
+  .start-des {
+    font-size: 13px;
+  }
 
-.start-des .fa {
-  width: 22px;
-}
+  .start-des .fa {
+    width: 22px;
+  }
 
-.start-des li {
-  margin-bottom: 10px;
-}
+  .start-des li {
+    margin-bottom: 10px;
+  }
 
-.item-container .el-menu {
-  width: 300px;
-}
+  .item-container .el-menu {
+    width: 300px;
+  }
 
-.item-container .el-menu a {
-  color: #bfcbd9;
-  text-decoration: none;
-  display: block;
-}
+  .item-container .el-menu a {
+    color: #bfcbd9;
+    text-decoration: none;
+    display: block;
+  }
 
-.run-dialog .el-checkbox {
-  display: block;
-  white-space: normal;
-}
+  .run-dialog .el-checkbox {
+    display: block;
+    white-space: normal;
+  }
 
-.run-dialog .icon-note {
-  color: #606266;
-}
+  .run-dialog .icon-note {
+    color: #606266;
+  }
 
-.module-header {
-  line-height: 60px;
-}
+  .module-header {
+    line-height: 60px;
+  }
 
-.module-aside {
-  padding: 20px;
-}
+  .module-aside {
+    padding: 20px;
+  }
 
-.module-aside .red {
-  color: red;
-}
+  .module-aside .red {
+    color: red;
+  }
 
-.collapse-transition {
-  -webkit-transition: 0s height, 0s padding-top, 0s padding-bottom;
-  transition: 0s height, 0s padding-top, 0s padding-bottom;
-}
+  .collapse-transition {
+    -webkit-transition: 0s height, 0s padding-top, 0s padding-bottom;
+    transition: 0s height, 0s padding-top, 0s padding-bottom;
+  }
 
-.horizontal-collapse-transition {
-  -webkit-transition: 0s width, 0s padding-left, 0s padding-right;
-  transition: 0s width, 0s padding-left, 0s padding-right;
-}
+  .horizontal-collapse-transition {
+    -webkit-transition: 0s width, 0s padding-left, 0s padding-right;
+    transition: 0s width, 0s padding-left, 0s padding-right;
+  }
 
-.horizontal-collapse-transition .el-submenu__title .el-submenu__icon-arrow {
-  -webkit-transition: 0s;
-  transition: 0s;
-  opacity: 0;
-}
+  .horizontal-collapse-transition .el-submenu__title .el-submenu__icon-arrow {
+    -webkit-transition: 0s;
+    transition: 0s;
+    opacity: 0;
+  }
 
-.start-log {
-  margin-top: 15px;
-}
+  .start-log {
+    margin-top: 15px;
+  }
 
-.el-card__header a {
-  text-decoration: none;
-}
+  .el-card__header a {
+    text-decoration: none;
+  }
 
-.box-card-tip {
-}
+  .box-card-tip {
+  }
 
-.box-card-des {
-  padding: 0.25em 0;
-  font-style: italic;
-  font-size: 12px;
-  color: #aab6aa;
-}
+  .box-card-des {
+    padding: 0.25em 0;
+    font-style: italic;
+    font-size: 12px;
+    color: #aab6aa;
+  }
 
-.thread-contenet {
-  margin-top: 10px;
-}
+  .thread-contenet {
+    margin-top: 10px;
+  }
 
-.start-title {
-  font-weight: 400;
-  font-size: 22px;
-  color: #1f2d3d;
-}
+  .start-title {
+    font-weight: 400;
+    font-size: 22px;
+    color: #1f2d3d;
+  }
 </style>
