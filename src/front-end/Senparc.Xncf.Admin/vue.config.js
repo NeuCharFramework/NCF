aca2bca54cdad6d7ca757c03cfc65510cf75dd49
LoganLan0 <hlan@senparc.com>
2023-12-14 16:48:25 +0800

    修改前端配置到developer分支上面的端口5001

--- src/front-end/Senparc.Xncf.Admin/vue.config.js
+++ src/front-end/Senparc.Xncf.Admin/vue.config.js
@@ -42,7 +42,7 @@ module.exports = {
     proxy: {
       // 所有的请求起始部分全部用 '/api'代替，比如访问"https://192.168.1.4/movie"，那么简写成"/api/movie"即可
       '/api': {
-        target: 'https://localhost:44311/api', // 开发域名
+        target: 'https://localhost:5001/api', // 开发域名
         // target: 'https://localhost:44311/api',// 正式域名
         changeOrigin: true,
         ws: true,
