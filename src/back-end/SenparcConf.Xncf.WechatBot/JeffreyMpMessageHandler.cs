40afeaca34344baec994e010d907d821addac17d
苏震巍 <zsu@senparc.com>
2023-12-29 21:56:58 +0800

    补充 WeChat AI 机器人直播内容

--- src/back-end/SenparcConf.Xncf.WechatBot/JeffreyMpMessageHandler.cs
+++ src/back-end/SenparcConf.Xncf.WechatBot/JeffreyMpMessageHandler.cs
@@ -14,7 +14,9 @@ using Senparc.Weixin.MP.MessageContexts;
 using Senparc.Weixin.MP.MessageHandlers;
 using Senparc.Xncf.WeixinManager;
 using Senparc.Xncf.WeixinManager.Models;
+using SenparcConf.Xncf.WechatBot;
 using System;
+using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
@@ -25,34 +27,10 @@ using System.Threading.Tasks;
 namespace Senparc.Web
 {
     [MpMessageHandlerAttribute("JeffreyMp")]
-    public class JeffreyMpMessageHandler : MessageHandler<DefaultMpMessageContext>
+    public class JeffreyMpMessageHandler : MessageHandler<DefaultMpMessageContext> //MessageHandler<WechatAiContext>
     {
         private readonly MpAccountDto _mpAccountDto;
         private readonly ServiceProvider _services;
-
-        private static Dictionary<string, IWantToRun> IWantoRunDic = new Dictionary<string, IWantToRun>();
-
-        private static IWantToRun GetIWantToRun(IServiceProvider services, PromptConfigParameter promptConfigParameter, string modelName, string openId)
-        {
-            if (IWantoRunDic.ContainsKey(openId))
-            {
-                return IWantoRunDic[openId];
-            }
-            else
-            {
-                SemanticAiHandler _semanticAiHandler = (SemanticAiHandler)services.GetRequiredService<IAiHandler>();
-
-
-                //配置和初始化模型
-                var chatConfig = _semanticAiHandler.ChatConfig(promptConfigParameter, userId: "Jeffrey",
-                                               modelName: modelName /*, modelName: "gpt-4-32k"*/);
-
-                var iWantToRun = chatConfig.iWantToRun;
-
-                IWantoRunDic[openId] = iWantToRun;
-                return IWantoRunDic[openId];
-            }
-        }
 
         //特殊的构造函数
         public JeffreyMpMessageHandler(MpAccountDto mpAccountDto, Stream stream, PostModel postModel, int maxRecordCount, ServiceProvider services) : this(stream, postModel, maxRecordCount)
@@ -63,19 +41,24 @@ namespace Senparc.Web
 
         public JeffreyMpMessageHandler(Stream inputStream, PostModel postModel, int maxRecordCount = 0, bool onlyAllowEncryptMessage = false, DeveloperInfo developerInfo = null, IServiceProvider serviceProvider = null) : base(inputStream, postModel, maxRecordCount, onlyAllowEncryptMessage, developerInfo, serviceProvider)
         {
+            this.GlobalMessageContext.MaxRecordCount = 10;
+            this.GlobalMessageContext.ExpireMinutes = 2;
         }
 
         public override async Task<IResponseMessageBase> OnTextRequestAsync(RequestMessageText requestMessage)
         {
-            var reponseMessage = this.CreateResponseMessage<ResponseMessageText>();
+          
             //reponseMessage.Content =  requestMessage.Content.Replace("?","!").Replace("？","！").Replace("吗","");
 
             var services = base.ServiceProvider;
 
             var smq = new SenparcMessageQueue();
-            smq.Add("WechatBot:" + requestMessage.MsgId,
+            var smqKey = "WechatBot:" + requestMessage.MsgId;
+            smq.Add(smqKey,
                 async () =>
                 {
+                    var dt = SystemTime.Now;
+
                     //定义 AI 请求参数
                     var parameter = new PromptConfigParameter()
                     {
@@ -88,16 +71,24 @@ namespace Senparc.Web
                     var modelName = "gpt-35-turbo";//text-davinci-003
 
                     //获取 AI 处理器
-                    var iWantToRun = GetIWantToRun(services, parameter, modelName, requestMessage.FromUserName);
+                    var iWantToRun = WechatAiContext.GetIWantToRun(services, parameter, modelName, requestMessage.FromUserName);
                     SemanticAiHandler semanticAiHandler = iWantToRun.SemanticAiHandler;
 
                     //发送到 AI 模型，获取结果
                     var result = await semanticAiHandler.ChatAsync(iWantToRun, requestMessage.Content);
 
+                    if (result==null)
+                    {
+                        await Console.Out.WriteLineAsync("result is null");
+                    }
+
                     //异步发送 AI 结果到用户
                     _ = Senparc.Weixin.MP.AdvancedAPIs.CustomApi.SendTextAsync(_mpAccountDto.AppId, requestMessage.FromUserName, result.Output);
+
+                    _ = Senparc.Weixin.MP.AdvancedAPIs.CustomApi.SendTextAsync(_mpAccountDto.AppId, requestMessage.FromUserName, $"总共耗时：{SystemTime.DiffTotalMS(dt)}ms");
                 });
 
+            var reponseMessage = this.CreateResponseMessage<ResponseMessageText>();
             reponseMessage.Content = "消息已收到，正在思考中……";
             return reponseMessage;
         }
