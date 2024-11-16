8fa6ee81a688faa57636b9de2c4d9c62349d1e08
JeffreySu <zsu@senparc.com>
2024-11-16 23:58:45 +0800

    优化 Apire 配置

--- src/back-end/Senparc.Aspire.AppHost/Program.cs
+++ src/back-end/Senparc.Aspire.AppHost/Program.cs
@@ -1,5 +1,21 @@
+using System.Text;
+
 var builder = DistributedApplication.CreateBuilder(args);
 
-var ncfWeb = builder.AddProject<Projects.Senparc_Web>("ncf-web");
+var ncfWeb = builder.AddProject<Projects.Senparc_Web>("ncf-web", "Senparc.Web");
+
+if (builder.ExecutionContext.IsRunMode)
+{
+    builder.AddProject("install", "../Senparc.Xncf.Installer/Senparc.Xncf.Installer.csproj", opt =>
+    {
+        opt.LaunchProfileName = "Senparc.Xncf.Installer";
+    });
+
+    //builder.AddContainer("my-redis", "redis", "5.0.14");
+}
+
+//支持中文字符
+Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
+Console.OutputEncoding = Encoding.GetEncoding("GB2312");
 
 builder.Build().Run();
