bf2498e28db5699367b7163cae02c2e48690f134
JeffreySu <zsu@senparc.com>
2025-05-20 22:03:09 +0800

    v0.29.1 升级 Senparc.Ncf.Core 核心模块

--- src/back-end/Senparc.Areas.Admin/Areas/Admin/Pages/TenantInfo/Index.cshtml.cs
+++ src/back-end/Senparc.Areas.Admin/Areas/Admin/Pages/TenantInfo/Index.cshtml.cs
@@ -58,7 +58,8 @@ namespace Senparc.Areas.Admin.Areas.Admin.Pages
         public async Task<IActionResult> OnGetListAsync(int pageIndex, int pageSize)
         {
             var tenantInfo = await _tenantInfoService.GetObjectListAsync(pageIndex, pageSize, z => true, z => z.Id, OrderingType.Ascending);
-            return Ok(new { 
+            return Ok(new
+            {
                 List = tenantInfo.AsEnumerable(),
                 TotalCount = tenantInfo.TotalCount,
                 PageIndex = tenantInfo.PageIndex
