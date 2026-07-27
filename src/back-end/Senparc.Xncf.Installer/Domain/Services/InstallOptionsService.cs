ffa5977665219e5e2994ce33eb23f1913b508443
JeffreySu <zsu@senparc.com>
2026-07-27 17:28:35 +0800

    Add Senparc.Xncf.DesktopBridge package reference and update InstallOptionsService to include its GUID

--- src/back-end/Senparc.Xncf.Installer/Domain/Services/InstallOptionsService.cs
+++ src/back-end/Senparc.Xncf.Installer/Domain/Services/InstallOptionsService.cs
@@ -42,7 +42,8 @@ namespace Senparc.Xncf.Installer.Domain.Services
             "C2E1F87F-2DCE-4921-87CE-36923ED0D6EA", // Senparc.Xncf.XncfBuilder
             "149D8021-1783-4FC9-97A8-F1A1BA60245B", // Senparc.Xncf.MCP
             "796D12D8-580B-40F3-A6E8-A5D9D2EABB69", // Senparc.Xncf.AIKernel
-            "D858D7FA-775A-4690-9023-CFB0B3B84994"  // Senparc.Xncf.AgentsManager
+            "D858D7FA-775A-4690-9023-CFB0B3B84994", // Senparc.Xncf.AgentsManager
+            "E9A9D870-A285-4BCE-9D4D-26A219C41252"  // Senparc.Xncf.DesktopBridge
         };
 
         private readonly IServiceProvider _serviceProvider;
